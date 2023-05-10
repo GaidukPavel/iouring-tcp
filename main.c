a22ad9f5bcb7da2cb64af5d2ae105d11366bc4fd
GaidukPavel <pav.gaiduk@gmail.com>
2023-05-11 02:59:03 +0700

    check

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "server.h"
 
 int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  
   int port = atoi(argv[1]);
   char fname[FILENAMESIZE];
   int outfd;
